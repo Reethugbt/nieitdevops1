96ee7be75347d8402375429e60a56380b9b089cf
Reethugbt <ritzgbt@gmail.com>
2023-02-03 15:18:24 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,9 +14,9 @@ function About() {
       <div className="about-nike-part">
         <h2 className="about-nike-heading">Abouuuuuuuuut Nike</h2>
         <p className='about-nike-para'>
-          Our misjjjjjjjsion is what drives us to do everything possible to expand
+          Our MISSION is what drives us to do everything possible to expand
           human potential. We do that by creating groundbreaking sport
-          innovations, by making our phhhhhhroducts more sustainably, by building a
+          innovations, by making our products more sustainably, by building a
           creative and diverse global team and by making a positive impact in
           communities where we live and work. Based in Beaverton, Oregon, NIKE,
           Inc. includes the Nike, Cojsrbvosrubosnverse, and Jordan brands.
